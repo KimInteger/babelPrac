8fce73906c5303fd8de2243d7c6f6ddf104325de
KimInteger <kjs20151240@gmail.com>
2024-07-02 11:56:01 +0900

    feat_김정수 : 에러처리

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,13 @@ const server = http.createServer((req,res)=>{
   if(req.method === 'GET'){
     res.writeHead(200,{"Content-Type":"text/plain"});
     res.end('This is me!');
+  } else {
+    res.writeHead(404, {"Content-Type" : "text/plain"});
+    res.end('404 Not-Found');
   }
 });
 
 server.listen(3000,()=>{
   console.log(`http://localhost:3000`);
 });
+
