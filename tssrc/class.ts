b9f3b87b1fd2fbe974e1e530ed6b269c9cfee4b5
KimInteger <kjs20151240@gmail.com>
2024-07-02 09:46:40 +0900

    feat_김정수 : add함수 추가

--- tssrc/class.ts
+++ tssrc/class.ts
@@ -2,3 +2,13 @@ function isIntegerCheck(value: number):boolean{
   let checkValue : boolean = Number.isInteger(value);
   return checkValue;
 }
+
+function add(a:number, b: number) : number {
+  if(isIntegerCheck(a) === false || isIntegerCheck(b) === false){
+    throw new Error('매개변수 둘 중 하나라도 정수가 아니라면 에러를 발생시키기');
+  }
+  let result : number = a + b;
+  return result
+}
+
+// 실행부분 ------------------------------------------------
