a1aa2e7523aa3612c042dfdebae4000e34ca92a7
KimInteger <kjs20151240@gmail.com>
2024-07-02 10:53:16 +0900

    feat_김정수 : 간단한 server open

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 import http from 'http';
 
+const server = http.createServer((req,res)=>{
+  if(req.method === 'GET'){
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.end('This is me!');
+  }
+});
+
+server.listen(3000,()=>{
+  console.log(`http://localhost:3000`);
+});
